96a046a1a5cc074dafeaa771ed706497faea13d9
Cesar Valderrama <valderramago@gmail.com>
2017-06-20 20:52:46 +0000

    Added back from edit contact and strong parameters for guest

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -7,6 +7,8 @@ class GuestsController < ApplicationController
 	has_scope :by_email
 
 	respond_to :html, :json
+	
+	after_filter :set_comming_from, only: :edit
 
 	def index
 		@guests = params[:q] ? Guest.where("name ILIKE :query OR surname ILIKE :query OR email ILIKE :query OR concat_ws(' ', name, surname) ILIKE :query", {query: "%#{params[:q]}%"}).page(params[:page]) : apply_scopes(Guest).page(params[:page])
@@ -16,6 +18,15 @@ class GuestsController < ApplicationController
 		end
 	end
 
+	def edit
+		@back_url = URI(request.referer || root_path).path
+		edit!
+	end
+	
+	def update
+		update!( notice: "Contacto actualizado exitosamente" ) { session[:previous_url] }	
+	end
+	
 	def create
 		@guest = Guest.new(guest_params)
     @guest.save
@@ -25,6 +36,10 @@ class GuestsController < ApplicationController
   private
 
     def guest_params
-      params.require(:guest).permit(:name, :surname, :email)
+      params.require(:guest).permit(:name, :surname, :email, :country, :city, :phone_number, :mobile_number, :identification, :civil_status, :profession, :nationality, :health_condition, :receive_drug, :symptoms, :contact_name, :contact_kin, :contact_number, :hotel, :local_number, :comments)
+    end
+    
+    def set_comming_from
+    	session[:previous_url] = URI(request.referer || '').path
     end
 end
