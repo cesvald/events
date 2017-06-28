02124e594c15bbe38b435af9baf7a5eb326786df
Cesar Valderrama <valderramago@gmail.com>
2017-06-28 06:02:10 +0000

    Removed temporally sparkpost initializer

--- config/initializers/sparkpost.rb
+++
@@ -1,7 +0,0 @@
-SparkPostRails.configure do |c|
-  c.api_key = Configuration[:sparkpost_key]
-  c.track_opens = true                            # default: false
-  c.track_clicks = true                      # default: false
-  c.inline_css = true
-  c.html_content_only = true  
-end
