37c5c2f6dd7eb7c6ed874c736d097d9668b600ec
Cesar Valderrama <valderramago@gmail.com>
2019-12-27 10:17:55 +0000

    changed participant by payable in logs of payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,10 +1,10 @@
 class Payment < ActiveRecord::Base
-  belongs_to :participant
   belongs_to :payable
   attr_accessor :author_id
   
   after_create :add_create_log
   before_destroy :add_destroy_log
+  
   after_update :add_update_log
   
   delegate :display_amount, to: :decorator
@@ -14,17 +14,17 @@ class Payment < ActiveRecord::Base
   end
   
   def add_create_log
-    is_reviewed = participant.payments.count == 1 && participant.air_ticket.nil? ? false : true
-    participant.change_logs.create(change: "creó el pago #{to_s}", author_id: author_id, is_reviewed: is_reviewed)
+    is_reviewed = payable.payments.count == 1 && payable.air_ticket.nil? ? false : true
+    payable.change_logs.create(change: "creó el pago #{to_s}", author_id: author_id, is_reviewed: is_reviewed)
   end
   
   def add_update_log
-    participant.change_logs.create(change: "actualizó el pago  #{to_s}", author_id: author_id)
+    payable.change_logs.create(change: "actualizó el pago  #{to_s}", author_id: author_id)
   end
   
   def add_destroy_log
-    is_reviewed = participant.payments.count == 1 && participant.air_ticket.nil? ? false : true
-    participant.change_logs.create(change: "eliminó el pago #{to_s}", author_id: author_id, is_reviewed: is_reviewed)
+    is_reviewed = payable.payments.count == 1 && payable.air_ticket.nil? ? false : true
+    payable.change_logs.create(change: "eliminó el pago #{to_s}", author_id: author_id, is_reviewed: is_reviewed)
   end
   
   def to_s
