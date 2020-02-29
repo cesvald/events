2980d8dcb34b7fa3915d708ddc003b1034e5d3c2
Cesar Valderrama <valderramago@gmail.com>
2020-02-29 18:19:52 +0000

    adds profile methods after pushing to heroku-kashi

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,10 @@ class Profile < ApplicationRecord
 	
 	has_and_belongs_to_many :users
 	
-	
+	Profile.all.pluck(:name).each do |name|
+		define_singleton_method name do
+			Profile.where(name: name).first
+		end
+	end
 	
 end
