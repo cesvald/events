01dca42da979713a3cf60ae29c70e67a1d220447
Cesar Valderrama <valderramago@gmail.com>
2022-05-14 00:24:11 -0500

    fixes params calendar view

--- app/assets/javascripts/views/locations/locations_calendar.js.coffee
+++ app/assets/javascripts/views/locations/locations_calendar.js.coffee
@@ -15,6 +15,7 @@ class Lodging.Views.LocationsCalendar extends Backbone.View
 		target.closest('.calendar-containor').find('.calendar-wrapper:first').hide()
 		target.removeClass('close-calendar').addClass('open-calendar')
 		target.closest('.calendar-containor').find('.glyphicon-minus').removeClass('glyphicon-minus').addClass('glyphicon-plus')
+		target.closest('.calendar-containor').find('.fa-minus-square-o').removeClass('fa-minus-square-o').addClass('fa-plus-square-o')
 		target.parent().find('.record-link:first').hide()
 	
 	openCalendar: ->
@@ -25,4 +26,5 @@ class Lodging.Views.LocationsCalendar extends Backbone.View
 		target.closest('.calendar-containor').find('.calendar-wrapper:first').show()
 		target.removeClass('open-calendar').addClass('close-calendar')
 		target.find('.glyphicon-plus').removeClass('glyphicon-plus').addClass('glyphicon-minus')
+		target.find('.fa-plus-square-o').removeClass('fa-plus-square-o').addClass('fa-minus-square-o')
 		target.parent().find('.record-link:first').show()
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,15 +6,17 @@ class LocationsController < BaseHostingController
   def calendar
     respond_to do |format|
       @locations = Location.includes(houses: [ rooms: [ :beds ] ]).all.order('houses.name ASC')
-  		params[:year] = Time.now.year if params[:year].blank?
-  		params[:month] = Time.now.month if params[:month].blank?
-  
-  		@start_date = Date.new(params[:year].to_i, params[:month].to_i, 1)
-  		@end_date = Date.new(params[:year].to_i, params[:month].to_i, -1)
+  		search_params[:year] = Time.now.year if search_params[:year].blank?
+  		search_params[:month] = Time.now.month if search_params[:month].blank?
+  		@start_date = Date.new(search_params[:year].to_i, search_params[:month].to_i, 1)
+  		@end_date = Date.new(search_params[:year].to_i, search_params[:month].to_i, -1)
+      
       format.xlsx {
         response.headers['Content-Disposition'] = "attachment; filename=\"Calendar #{@start_date.strftime('%B %Y')}.xlsx\""
       }
-      format.html
+      format.html {
+        @search_params = params.require(:date).permit(:month, :year)
+      }
     end
 	end
 
@@ -24,6 +26,9 @@ class LocationsController < BaseHostingController
       params.require(:location).permit(:name)
     end
 
+    def search_params
+      params.require(:date).permit(:month, :year)
+    end
   
 end
 
